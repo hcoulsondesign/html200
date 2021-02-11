821454b8af1966406c7ce52f09f540e377aac7ff
HCoulson <HCoulson@users.noreply.github.com>
2021-02-10 20:35:58 -0800

    completed states array

---
+++ lesson03/states.js
@@ -0,0 +1,15 @@
+let ausStates = ['Queensland', 'New South Wales', 'Victoria', 'Northern Territory', 'Western Australia'];
+
+console.log(ausStates);
+
+ausStates.push('Tasmania');
+
+console.log(ausStates);
+
+ausStates.splice(2,1);
+
+console.log(ausStates);
+
+const statesList = ausStates.join(',');
+
+console.log(statesList);
