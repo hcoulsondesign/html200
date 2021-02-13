211d5472872b39a558c879e937c4211763339345
HCoulson <HCoulson@users.noreply.github.com>
2021-02-13 12:00:04 -0800

    syncing latest changes before next assignment

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,10 @@
+const twelveDays = ['my true love gave to me','partridge in a pear tree', 'turtle doves', 'French hens', 'calling birds', 'GOOOOOLD rings'];
+
+for (i = 0; i < twelveDays.length; i++) {
+  console.log(i + ',' + twelveDays[i]);
+}
+
+//just wanted to see if I could get this to work
+for (i = twelveDays.length-1; i>=0; i--) {
+  console.log(i + ',' + twelveDays[i]);
+}
