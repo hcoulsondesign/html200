036edf84deef5e618282ed37d9742a42035f6c80
HCoulson <HCoulson@users.noreply.github.com>
2021-02-14 18:21:34 -0800

    coded simple functions

---
+++ lesson04/challenge1.js
@@ -0,0 +1,20 @@
+// console.log('it is working');
+
+function square() {
+  let input = prompt('Enter a number');
+  let result = 0;
+
+  result = Number(input) * Number(input);
+
+  alert(result);
+}
+
+function multiply() {
+  let input1 = prompt('Enter your first number');
+  let input2 = prompt('Enter another number');
+  let product = 0;
+
+  product = Number(input1) * Number(input2);
+
+  alert(product);
+}
